fef9361200e43acc4d2044dadc965510d0957c86
Peter Yang <me@peteryang.io>
2022-11-03 09:33:23 +0000

    regexdos example

--- frontend/app/Nav.tsx
+++ frontend/app/Nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BellIcon, HomeIcon, MailIcon, PersonIcon } from "@primer/octicons-react";
+import { BellIcon, HomeIcon, MailIcon, PersonIcon, ShieldXIcon } from "@primer/octicons-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -24,19 +24,33 @@ const links = [
         text: "Profile",
         icon: <PersonIcon />,
         href: "/profile"
+    },
+    {
+        text: "RegEx DoS",
+        icon: <ShieldXIcon />,
+        href: "/regex-dos"
     }
 ];
+
+function isActiveLink(pathname: string, href: string) {
+    if (href === "/" && pathname === "/") {
+        return true;
+    }
+
+    return href !== "/" && pathname.startsWith(href);
+}
 
 export default function Nav() {
     const pathname = usePathname();
 
     return (
         <nav className="Layout-sidebar SideNav border">
-            {links.map((link, index) => (
+            {links.map(link => (
                 <Link
+                    key={link.href}
                     className="SideNav-item menu-item"
                     href={link.href}
-                    aria-current={pathname === link.href ? "page" : undefined}
+                    aria-current={isActiveLink(pathname, link.href) ? "page" : undefined}
                 >
                     {link.icon}
                     {link.text}
